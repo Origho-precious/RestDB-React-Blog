d5964ff30219b650ad7fa82725ad354632b43c10
Origho-precious <Origho9@gmail.com>
2021-09-29 23:19:31 +0100

    feat: adding Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ children, type = "button", className, ...props }) => {
+	return (
+		<button
+			className={`${className} block text-black h-12 px-6 rounded-md font-semibold hover:opacity-75 transition-opacity duration-500 ease-in`}
+			type={type}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
