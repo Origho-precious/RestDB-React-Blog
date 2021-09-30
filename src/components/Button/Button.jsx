9fef11c18f320fd10a0b1cbcfbde3d875bad3d3c
Origho-precious <Origho9@gmail.com>
2021-09-30 13:04:04 +0100

    feat: updating Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,16 @@
-const Button = ({ children, type = "button", className, ...props }) => {
+const Button = ({
+	children,
+	outline,
+	type = "button",
+	className,
+	...props
+}) => {
 	return (
 		<button
+			style={{
+				background: outline ? "#242424" : "",
+				color: outline ? "#2eff7b" : "#000",
+			}}
 			className={`${className} block text-black px-6 rounded-md font-semibold hover:opacity-75 transition-opacity duration-500 ease-in`}
 			type={type}
 			{...props}
