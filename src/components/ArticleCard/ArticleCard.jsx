a30eb60c1591b5eaa8878e4499f2bfca87b87d3a
Origho-precious <Origho9@gmail.com>
2021-10-10 21:40:54 +0100

    feat: refactoring ArticleCard and creating helper functions

---
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -0,0 +1,25 @@
+import { calcReadTime, truncateText } from "../../utils/helperFunctions";
+
+const PostCard = ({ id, title, body, timeStamp, tags }) => {
+	return (
+		<div
+			title={title}
+			className="flex flex-col justify-between bg-black h-48 py-8 px-12 rounded-md"
+		>
+			<div>
+				<h3 className="font-bold text-2xl mb-4">{truncateText(title, 37)}</h3>
+				<div className="flex">
+					{tags?.map((tag) => (
+						<p className="mr-4 opacity-80 text-white text-sm">#{tag}</p>
+					))}
+				</div>
+			</div>
+			<div className="flex items-center justify-between">
+				<p>Oct 10th (4 hours ago)</p>
+				<p>{calcReadTime(body)}</p>
+			</div>
+		</div>
+	);
+};
+
+export default PostCard;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,14 @@
+import ArticleCard from "../../components/ArticleCard/ArticleCard";
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<section className="w-1/2 mx-auto">
+			{/* <ArticleCard
+				title="Building a blog with React and RestDB for openreplay blog"
+				tags={["React", "RestDB", "Nextjs", "Node"] body={'body}}
+			/> */}
+		</section>
+	);
 };
 
 export default Home;
