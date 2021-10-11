ddf265efe48f5c53e5785eb366fa5271ef555234
Origho-precious <Origho9@gmail.com>
2021-10-11 21:52:22 +0100

    feat: updating homepage

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import {
 	calcReadTime,
 	convertDate,
@@ -6,25 +7,27 @@ import {
 
 const PostCard = ({ id, title, body, timeStamp, tags }) => {
 	return (
-		<div
-			title={title}
-			className="flex flex-col justify-between bg-black h-48 py-8 px-12 rounded-md"
-		>
-			<div>
-				<h3 className="font-bold text-2xl mb-4">{truncateText(title, 37)}</h3>
-				<div className="flex">
-					{tags?.map((tag, idx) => (
-						<p key={tag + idx} className="mr-4 opacity-80 text-white text-sm">
-							#{tag}
-						</p>
-					))}
+		<Link to={`/article/${id}`}>
+			<div
+				title={title}
+				className="flex flex-col justify-between bg-black h-48 py-8 px-12 rounded-md hover:bg-gray-900 transition-all duration-700"
+			>
+				<div>
+					<h3 className="font-bold text-2xl mb-4">{truncateText(title, 37)}</h3>
+					<div className="flex">
+						{tags?.map((tag, idx) => (
+							<p key={tag + idx} className="mr-4 opacity-80 text-white text-sm">
+								#{tag}
+							</p>
+						))}
+					</div>
+				</div>
+				<div className="flex items-center justify-between">
+					<p>{convertDate(timeStamp)}</p>
+					<p>{calcReadTime(body)}</p>
 				</div>
 			</div>
-			<div className="flex items-center justify-between">
-				<p>{convertDate(timeStamp)}</p>
-				<p>{calcReadTime(body)}</p>
-			</div>
-		</div>
+		</Link>
 	);
 };
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,13 +4,17 @@ import axios from "../../utils/api.client";
 
 const Home = () => {
 	const [articles, setArticles] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	const fetchArticles = async () => {
+		setLoading(true);
 		try {
 			const res = await axios.get("/articles");
 			setArticles(res?.data);
+			setLoading(false);
 		} catch (error) {
 			console.log(error);
+			setLoading(false);
 		}
 	};
 
@@ -20,17 +24,23 @@ const Home = () => {
 
 	return (
 		<section className="w-1/2 mx-auto">
-			{articles?.map((article) => (
-				<article key={article?._id} className="mb-4">
-					<ArticleCard
-						id={article?._id}
-						title={article?.title}
-						tags={article?.tags?.split(",")}
-						body={article?.body}
-						timeStamp={article?.timestamp}
-					/>
-				</article>
-			))}
+			{loading ? (
+				<p className="text-center">Loading...</p>
+			) : articles?.length ? (
+				articles?.map((article) => (
+					<article key={article?._id} className="mb-4">
+						<ArticleCard
+							id={article?._id}
+							title={article?.title}
+							tags={article?.tags?.split(",")}
+							body={article?.body}
+							timeStamp={article?.timestamp}
+						/>
+					</article>
+				))
+			) : (
+				<p className="text-center">No article, create post</p>
+			)}
 		</section>
 	);
 };
